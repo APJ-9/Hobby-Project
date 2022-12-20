1aa25b57ac0499c3349fc16d8a93b9af12ca04e1
APJ <akhil.jose@vonnue.com>
2022-12-20 17:29:32 +0530

    feat: todo list

--- javascript-course-udmey/section-50/script.js
+++ javascript-course-udmey/section-50/script.js
@@ -10,14 +10,17 @@ if(todos){
 
 form.addEventListener('submit',(e)=>{
     e.preventDefault()
+
     addTodo()
 })
 
 function addTodo(todo){
     let todoText = input.value
+
     if(todo){
         todoText = todo.text
     }
+
     if(todoText){
         const todoEl = document.createElement('li')
         if(todo && todo.completed){
@@ -28,6 +31,35 @@ function addTodo(todo){
 
         todoEl.addEventListener('click',()=>{
             todoEl.classList.toggle('completed')
+            updateLS()
         })
+
+        todoEl.addEventListener('contextmenu',(e) => {
+            e.preventDefault()
+
+            todoEl.remove()
+            updateLS()
+        })
+
+        todosUL.appendChild(todoEl)
+
+        input.value = ''
+
+        updateLS()
     }
+}
+
+function updateLS() {
+    todosEl = document.querySelectorAll('li')
+
+    const todos = []
+
+    todosEl.forEach(todoEl => {
+        todos.push({
+            text: todoEl.innerText,
+            completed: todoEl.classList.contains('completed')
+        })
+    })
+
+    localStorage.setItem('todos',JSON.stringify(todos))
 }
