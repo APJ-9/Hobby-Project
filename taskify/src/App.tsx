fa0cd0a1595378157aaf861993721244d57432eb
APJ <akhil.jose@vonnue.com>
2022-12-21 11:00:50 +0530

    feat: css

--- taskify/src/App.tsx
+++ taskify/src/App.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { WatchDirectoryFlags } from 'typescript';
 
-function App() {
+const App :React.FC = () => {
   return (
     <div className="App">
-      Hello World
+      <span className='heading'>Taskify</span>
+
     </div>
   );
 }
 
 export default App;
+
+
+//unknown another way of any 
+//void and never --> void returns undefined but never returns nothing
+//type and interface
